6b4f950ea8c7b4ea0a825907f08f2987f95e7af3
Andreas Henneberger <andreas.henneberger@mytum.de>
2014-03-07 17:24:33 +0100

    abgeschlossen

---
+++ sekunden_31.cpp
@@ -0,0 +1,33 @@
+/* 
+ * File:   sekunden.cpp
+ * Author: AndiPower
+ *
+ * Created on 7. März 2014, 16:27
+ */
+
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+int dauerInSekunden(int stunden, int minuten, int sekunden);
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    int stunden, minuten, sekunden, sekunden_gesamt;
+    cout << "Bitte geben Sie die Zeit in Stunden, Minuten und Sekunden ein:\n";
+    cin >> stunden >> minuten >> sekunden;
+    sekunden_gesamt = dauerInSekunden(stunden, minuten, sekunden);
+    cout << stunden << " Stunden " << minuten << " Minuten " << sekunden << " Sekunden " << " sind ingesamt " << sekunden_gesamt << " Sekunden" << endl;
+    return 0;
+}
+
+int dauerInSekunden(int stunden, int minuten, int sekunden)
+{
+    int erg;
+    erg = 3600*stunden+60*minuten+sekunden;
+    return erg;
+}
+
